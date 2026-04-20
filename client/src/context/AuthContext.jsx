625a993b953dec6d720af8df0af53049382cae7a
Md. Al Amin <alaminsarkar863@gmail.com>
2026-04-20 22:25:39 +0600

    fixing usestate related issues

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { clearCache } from '../lib/apiCache';
 
 const AuthContext = createContext();
